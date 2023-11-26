0afee8122ba931c4f68d8597a63e3b996c0bad48
akash lowanshi <akashlowanshi86126@gmail.com>
2023-11-26 23:47:40 +0530

    first commit

--- models/db.js
+++ models/db.js
@@ -4,6 +4,6 @@ mongoose.set('strictQuery', false);
 
 mongoose
     // .connect("mongodb://127.0.0.1:27017/whatscAppClone")
-    .connect("mongodb+srv://akashlowanshi86126:h91hOV6rJkJjZLNE@cluster0.ggu19ho.mongodb.net/?retryWrites=true&w=majority")
+    .connect(process.env.mongodb_URL)
     .then(() => console.log("db connected!"))
     .catch((err) => console.log(err.message));
